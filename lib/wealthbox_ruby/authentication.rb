1afe38b8ddfd64d8ef532929dd0e546d1e9f96d2
Christopher Ostrowski <chris@madebyfunction.com>
2018-01-28 21:54:18 -0500

    Added class method to generate an API token from wealthbox

--- lib/wealthbox_ruby/authentication.rb
+++ lib/wealthbox_ruby/authentication.rb
@@ -1,5 +1,9 @@
 module Wealthbox
   module Authentication
+    def self.included(base)
+        base.extend(ClassMethods)
+      end
+
     def refresh_access_token!
       raise ArgumentError, 'Refresh token required.' unless @refresh_token
 
@@ -21,6 +25,15 @@ module Wealthbox
         oauth_token: response[:access_token],
         refresh_token: response[:refresh_token]
       }
+    end
+
+    module ClassMethods
+      def create_api_token(username:,password:)
+        HTTParty.post("#{Wealthbox.config.api_endpoint}/authentication", body: {
+          email: username,
+          password: password
+        })
+      end
     end
   end
 end
