f7f47b2555de5c16c5a59b24f73e92c6a10f935e
Ben Downey <Ben.Downey@MutuallyHuman.com>
2019-01-23 20:50:48 -0800

    APIModel#json_params: add logic to coerce DateTime to properly formatted string This fixes a bug where DateTime objects were being coerced into Dates, which caused trouble when creating Dates through the API.

--- lib/wealthbox_ruby/models/api_model.rb
+++ lib/wealthbox_ruby/models/api_model.rb
@@ -15,6 +15,10 @@ module WealthboxRuby
           next params if val.is_a?(Array) && val.length == 0
           next params if val.is_a?(Hash) && !val.keys.any?
 
+          if val.is_a?(DateTime)
+            val = val.strftime('%Y-%m-%dT%H:%M:%S%z')
+          end
+
           if val.is_a?(Date)
             val = val.strftime('%F')
           end
